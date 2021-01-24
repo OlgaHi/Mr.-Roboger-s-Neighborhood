156f85024bd65ad31546bc3765c031079f98ac27
Olha Hizhytska <olgainfotech@gmail.com>
2021-01-24 11:22:32 -0800

    Add user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,35 @@
 let result = [];
 
-function beepBoop(num) {
-  if (isNaN(num) || num < 0) {
-   console.log(false);
-  } else { 
-    for (let i=0; i <= num; i++) {
-     result.push(i.toString());
-    
-      if (result[i].includes("3")){
-      result[i] = "Won't you be my neighbor?";
-      } else if (result[i].includes("2")) {
-      result[i] = "Boop";
-      } else if (result[i].includes("1")){
+function beepBoop(number) {
+  for (let i=0; i <= number; i++) {
+    result.push(i.toString());
+
+    if (result[i].includes("3")){
+    result[i] = "Won't you be my neighbor?";
+    } else if (result[i].includes("2")) {
+    result[i] = "Boop";
+    } else if (result[i].includes("1")){
       result[i] = "Beep";
-      }
     }
-  }
-    console.log(result);
+   }
+  return result.toString();
 }
-beepBoop(12);
+
+
+$(document).ready(function() {
+  $("form#transform_number").submit(function(event) {
+    event.preventDefault();
+    const number = $("#number").val();
+
+    if (isNaN(number) || number < 0) {
+      $("#alert").show()
+    } else {
+      const displayResult = beepBoop(number)
+      console.log(displayResult)
+      $("#displayResult").append(displayResult)
+    };
+  })
+}
+)
  
 
